71ee44cd239d8ddc36e1ab59b27e7ec5bd7eb87e
DheoAp <dheoap21@gmail.com>
2021-05-08 09:20:36 +0700

    route kategori dan tag

--- routes/web.php
+++ routes/web.php
@@ -2,32 +2,34 @@
 
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::GET('/', function () {
+  return view('welcome');
 });
 
 
 // Blog
-Route::get('/blog',[BlogController::class, 'index']);
-Route::get('/create',[BlogController::class, 'create']);
-  Route::post('/store',[BlogController::class, 'store']);
-Route::get('/blog/{blog:slug}',[BlogController::class, 'show']);
-Route::get('/blog/edit/{blog:slug}',[BlogController::class, 'edit']);
+Route::GET('/blog',[BlogController::class, 'index']);
+Route::GET('/create',[BlogController::class, 'create']);
+  Route::POST('/store',[BlogController::class, 'store']);
+Route::GET('/blog/{blog:slug}',[BlogController::class, 'show']);
+Route::GET('/blog/edit/{blog:slug}',[BlogController::class, 'edit']);
   Route::PATCH('/blog/edit/{blog:slug}',[BlogController::class, 'update']);
 Route::DELETE('/blog/hapus/{blog:slug}',[BlogController::class, 'delete']);
 
 // Kategori
-Route::get('/kategori/list',[KategoriController::class, 'index']);
-Route::post('/kategori/proses',[KategoriController::class, 'store' ]);
-Route::get('/kategori/{kategori:slug}',[KategoriController::class, 'show']);
-Route::get('/kategori/edit/{kategori:slug}',[KategoriController::class, 'edit']);
-  Route::patch('/kategori/edit/{kategori:slug}',[KategoriController::class, 'update']);
-Route::get('/kategori/delete/{kategori:slug}',[KategoriController::class, 'alertDelete']);
-  Route::delete('/kategori/delete/{kategori:slug}',[KategoriController::class, 'delete']);
+Route::GET('/kategori/list',[KategoriController::class, 'index']);
+Route::POST('/kategori/proses',[KategoriController::class, 'store' ]);
+Route::GET('/kategori/{kategori:slug}',[KategoriController::class, 'show']);
+Route::GET('/kategori/edit/{kategori:slug}',[KategoriController::class, 'edit']);
+  Route::PATCH('/kategori/edit/{kategori:slug}',[KategoriController::class, 'update']);
+Route::GET('/kategori/delete/{kategori:slug}',[KategoriController::class, 'alertDelete']);
+  Route::DELETE('/kategori/delete/{kategori:slug}',[KategoriController::class, 'delete']);
 
 // Tag
-Route::get('/tag', function () {
-  return 'Halaman Tag';
-});
+Route::GET('/tag',[TagController::class, 'index']);
+Route::POST('/tag/proses',[TagController::class, 'store']);
+Route::GET('/tag/hapus/{tag:slug}',[TagController::class, 'alertDelete']);
+  Route::DELETE('/tag/hapus/{tag:slug}',[TagController::class, 'delete']);
